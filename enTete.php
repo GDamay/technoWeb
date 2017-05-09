74e7d137459c9f15ae3b0e46452b061cf8c7db76
PhO <hotom95@gmail.com>
2017-05-09 16:10:58 +0200

    modifications

--- enTete.php
+++ enTete.php
@@ -1,6 +1,6 @@
 <table>
 <tr>
-	<td><a href="accueil.php">Accueil</a><td>
+	<td><a href="accueil.html">Accueil</a><td>
 	<td><a href="evenements.php">Evenements</a><td>
 	<td><a href="inscription.php">Inscription</a><td>
 	<td><a href="contact.php">Contact</a><td>
