bc9466ff535eee0423c3e0fd10cc4c43d087c35b
PhO <hotom95@gmail.com>
2017-05-09 11:37:17 +0200

    création d'enTete.php

---
+++ enTete.php
@@ -0,0 +1,8 @@
+<table>
+<tr>
+	<td><a href="accueil.html">Accueil</a><td>
+	<td><a href="evenements.php">Evenements</a><td>
+	<td><a href="inscription.php">Inscription</a><td>
+	<td><a href="contact.php">Contact</a><td>
+</tr>
+</table>
