1b25436022721f27e28544d33f12af823db7189c
virgiliaL <jianglei.wang@insa-rouen.fr>
2017-05-12 11:17:20 +0200

    En train de modifier CSS

--- panier.php
+++ panier.php
@@ -2,16 +2,39 @@
 <HTML>
 <head>
     <link rel="stylesheet" type="text/css" href="main.css">
-	<title>Contact</title>
+	<title>Panier</title>
 	<meta charset="UTF-8"/>
 	<link rel="icon" type="image/x-icon" href="favicon.ico" />
+<!--<style>
+table {
+    border-collapse: collapse;
+    width: 100%;
+}
+
+th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+}
+
+tr:hover{background-color:#f5f5f5}
+</style>-->
+<style>
+fieldset
+{
+	border:2px solid green;
+    -moz-border-radius:8px;
+    -webkit-border-radius:8px;	
+    border-radius:8px;	
+}
+</style>
 </head>
 <body>
 	<header>
 		<?php include("enTete.php"); ?>
 	</header>
 	<?php
-	$fichier = fopen ("evenementsEnre", "r");
+	    	$fichier = fopen ("evenementsEnre", "r");
 	    	//fputs ( $fichier , "Une phrase\n");
 	    	/* sous Windows \r\n, sous Linux ou mac \n*/
 	/*   	echo "<p> Dans le fichier fichier . txt :</p>"; */
@@ -20,34 +43,37 @@
 	    	while ($event!=false)
 				{
 						list($titre, $lieu, $date,$description)=explode("|",$event);
+		
+
 						$event=fgets($fichier);
 	      }
 	    fclose ( $fichier );
-		?>
+	    ?>
+	
 	<section>
-		<table>
+		<table class="fullPage">
 			<tr>
-				<td><fieldset>
+				<td><colConnexion><fieldset>
 					<legend>Evénéments</legend>
-					<label><strong>$titre:</strong></label><br/>
-					<label>$date </label><br/>
-					<label>$lieu </label><br/>
-				</fieldset></td>
-				<td colspan="2" rowspan="2"><fieldset>
+					<label><?php echo"<strong>$titre</strong>"?></label><br/>
+					<label><?php echo $date ?> </label><br/>
+					<label><?php echo $lieu ?></label><br/>
+				</fieldset></colConnexion></td>
+				<td colspan="2" rowspan="2"><fieldset id="fieldsetPanier">
 					<legend>Painer</legend>
 					<table>
 						<thead>
 						<tr>
-							<th>Tarif</th>
-							<th>Prix</th>
-							<th>Quantité</th>
+							<th id="pr">Tarif</pr></th>
+							<th id="pr">Prix</pr></th>
+							<th><pr id="pr">Quantité</pr></th>
 						</tr>
 						</thead>
 						<tbody>
 						<tr>
-							<td>$titre</td>
-							<td>20 €</td>
-							<td><select>
+							<td id="pr"><?php echo $titre ?></td>
+							<td id="pr">20 €</td>
+							<td id="pr"><select>
 								<option value= "1">1</option>
 								<option value= "2">2</option>
 								<option value= "3">3</option>
@@ -66,7 +92,7 @@
 				<td><fieldset>
 					<legend>Contact Organisateur</legend>
 					<label>INSA de Rouen, </label><br/>
-					<label>non loin d'un ordinateur et </label><br/>
+					<label>non loin d''un ordinateur et </label><br/>
 					<label>de cafféine ou de boisson sucrée.</label><br/>
 				</fieldset></td>
 			</tr>
