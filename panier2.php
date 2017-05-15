6d0f5c24dce67a1e222e299b603ab0e9952c569b
virgiliaL <jianglei.wang@insa-rouen.fr>
2017-05-15 18:50:31 +0200

    Mettre les styles dans main.css

--- panier2.php
+++ panier2.php
@@ -6,12 +6,7 @@
 	<title>Panier</title>
 	<meta charset="UTF-8"/>
 	<link rel="icon" type="image/x-icon" href="favicon.ico" />
-<style>
-table {
-    border-collapse: collapse;
-    width: 100%;
-}
-</style>
+
 </head>
 <body>
 	<header>
@@ -43,15 +38,15 @@ table {
 							fclose ( $fichier );
 							?>
 				</fieldset></td>
-				<td colspan="2" rowspan="2"><fieldset id='boitePanier' style="width:100%;margin-top:0;">
+				<td colspan="2" rowspan="2" style="vertical-align: top;"><fieldset id='boitePanier' style="width:100%;margin-top:0;">
 					<legend id='titrePanier'>Panier</legend>
 					
-					<table>
+					<table id='tablePanier'>
 						<thead>
 						<tr style="tr:hover{background-color:#f5f5f5}">
-							<th style="padding: 8px;text-align: left;border-bottom: 1px solid #ddd;">Tarif</th>
-							<th style="padding: 8px;text-align: left;border-bottom: 1px solid #ddd;">Quantité</th>
-							<th style="padding: 8px;text-align: left;border-bottom: 1px solid #ddd;">Prix</th>
+							<th id='tdPanier'>Tarif</th>
+							<th id='tdPanier'>Quantité</th>
+							<th id='tdPanier'>Prix</th>
 						</tr>
 						</thead>
 						<tbody>
@@ -69,8 +64,8 @@ table {
 								echo "<td style='padding: 8px;text-align: left;border-bottom: 1px solid #ddd;'>$titre</td>";
 								?>
 
-								<td style='padding: 8px;text-align: left;border-bottom: 1px solid #ddd;'><?php echo $_POST['selectTab'][$i] ?></td>
-								<td style='padding: 8px;text-align: left;border-bottom: 1px solid #ddd;'><?php echo $_POST['selectTab'][$i]*20 ?>€</td>
+								<td id='tdPanier'><?php echo $_POST['selectTab'][$i] ?></td>
+								<td id='tdPanier'><?php echo $_POST['selectTab'][$i]*20 ?>€</td>
 								<?php
 								$total=$total+$_POST['selectTab'][$i]*20;
 								$i=$i+1;
@@ -87,8 +82,9 @@ table {
 							</tr>
 							<tr>
 							<td><br/></td>
-							<td><br/></td>
 							<td style="text-align: left"><input type="submit" /></td>
+							<form action="panier.php">
+							<td style="text-align: left"><input type="submit" value="retourner" /></td>
 						</tr>
 						</tbody>
 					</table>
